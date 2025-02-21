5acf996a8ba4381fd1c467a515ef9223fa7756ec
duvan-23 <duvamendi2@hotmail.com>
2025-02-20 21:12:23 -0500

    initial survey data

---
+++ src/constants/dataSurvey.tsx
@@ -0,0 +1,83 @@
+const initialData = [
+    {
+        age: "23",
+        gender: "M",
+        hasLicense: "true",
+        isFirstCar: "false",
+        drivetrain: "fwd",
+        fuelEmissionsConcern: "yes",
+        familyCars: "2",
+        carMake: [
+            "Honda",
+            "Toyota"
+        ],
+        carModel: [
+            "Amaze",
+            "Corola"
+        ]
+    },
+    {
+        age: "16",
+        gender: "F",
+        hasLicense: "true",
+        isFirstCar: "false",
+        drivetrain: "rwd",
+        fuelEmissionsConcern: "false",
+        familyCars: "2",
+        carMake: [
+            "Honda",
+            "Toyota"
+        ],
+        carModel: [
+            "Amaze",
+            "Sienna"
+        ]
+    },
+    {
+        age: "33",
+        gender: "M",
+        hasLicense: "true",
+        isFirstCar: "false",
+        drivetrain: "idk",
+        fuelEmissionsConcern: "yes",
+        familyCars: "1",
+        carMake: [
+            "Toyota"
+        ],
+        carModel: [
+            "Prius"
+        ]
+    },
+    {
+        age: "17",
+        gender: "M",
+        hasLicense: "false",
+        isFirstCar: null,
+        drivetrain: "",
+        fuelEmissionsConcern: "",
+        familyCars: "",
+        carMake: [
+            ""
+        ],
+        carModel: [
+            ""
+        ]
+    },
+    {
+        age: "34",
+        gender: "F",
+        hasLicense: "true",
+        isFirstCar: "true",
+        drivetrain: "",
+        fuelEmissionsConcern: "",
+        familyCars: "",
+        carMake: [
+            ""
+        ],
+        carModel: [
+            ""
+        ]
+    }
+];
+
+export { initialData };
